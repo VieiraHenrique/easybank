f2507a206efa0ddd3391bed2d6bf70e412686987
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2020-12-18 21:52:17 +0100

    fix button

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('menu');
-const overlay = document.getElementById('overlay');
-
-hamburger.addEventListener('click', () => {
-	menu.classList.toggle('close');
-	hamburger.classList.toggle('x');
-	overlay.classList.toggle('overlay-hidden');
-});
